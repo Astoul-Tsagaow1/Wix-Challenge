5f39cbefa34199988b3277988db576158d86fa40
Astoul <astul053@gmail.com>
2020-08-12 23:03:48 +0300

    reactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,39 @@
 import React from "react";
-// import"./app.css"  
+// import"./app.css"
 
-import GetCarsList from './components/GetCarListAndDisplay';
-import { Typography } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
+import GetCarsList from "./components/GetCarListAndDisplay";
+import { Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 function App() {
   return (
     <div className="App">
-         <Box bgcolor="primary.main" color="primary.contrastText" p={5} >
-          primary.main
-        </Box>  
-        <Typography  >
-        <Box letterSpacing={6} fontSize="2.75rem"  variant="h2"align="center" fontFamily="Roboto" >
-          
+      <Typography>
+        <Box
+          bgcolor="primary.main"
+          fontSize="2.3rem"
+          fontFamily="Roboto"
+          color="white "
+          p={5}
+        >
+          AsCars <b>Wix challenge</b>
+        </Box>{" "}
+      </Typography>
+
+      <Typography>
+        <Box
+          letterSpacing={6}
+          fontSize="2.75rem"
+          variant="h2"
+          align="center"
+          fontFamily="Roboto"
+        >
           Cars Catalog
-        
-          </Box>
-        
-        
-        
-        </Typography>
+        </Box>
+      </Typography>
 
-      <GetCarsList/>
+      <GetCarsList />
     </div>
-
   );
 }
 //
 export default App;
-
